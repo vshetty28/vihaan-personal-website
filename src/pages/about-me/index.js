c5a59f415381f76ac29e1b072f00d95b86c7cab2
vshetty28 <vshetty1028@gmail.com>
2025-02-04 04:24:42 -0500

    basic about-me page created

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 const AboutMe = () => {
-  return (
-    <div>
-      About Me
-    </div>
-  )
-}
+	return (
+		<div>
+			<div className="hero bg-base-200 w-screen">
+				<div className="hero-content flex-col lg:flex-row-reverse">
+					<Image src="/assets/headshot.png" alt='' class="size-1/3 rounded-lg shadow-2xl" width={3000} height={4000}/>
+					<div className="">
+						<h1 className="text-5xl font-bold">About Me</h1>
+						<p className="py-6">Hi! My name is Vihaan Shetty. Welcome to my personal website! I am originally from the SF Bay Area, California, but studying Computer Science and Data Science at Purdue. I am a huge sports fan, with some of my favorite teams being the 49ers, Warriors, FC Barcelona and Liverpool FC. In my free time, I love working out and playing videogames. Feel free to contact me at the links provided below!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default AboutMe
+export default AboutMe;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+			<main className="bg-base-200 flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Hello</h1>
       </main>
 	);
