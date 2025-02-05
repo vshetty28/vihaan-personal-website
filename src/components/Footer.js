ee8d3cc46139803787d37d795aa09e07d031ddc7
vshetty28 <vshetty1028@gmail.com>
2025-02-05 03:06:03 -0500

    Changed linkedin Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
 					<a href="mailto:vihaandhshetty@gmail.com">
 						<Image src="/assets/email.svg" alt="" className="size-12 lg:size-14 hover:scale-110 lg:mt-2 transition delay-0 duration-200 ease-in-out" width={50} height={80} />
 					</a>
-					<a href="https://linkedin.com/in/vihaanshetty" target="_blank">
+					<a href="www.linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=vihaanshetty" target="_blank">
 						<Image src="/assets/linkedin.svg" alt="" className="size-8 lg:size-10 mt-2 mb-3 lg:mb-0 lg:mt-4 transition delay-0 duration-200 ease-in-out hover:scale-110 " width={50} height={50} />
 					</a>
 				</div>
