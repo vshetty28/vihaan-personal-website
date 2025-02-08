f6621f0feb0cab143fc25dbde8e5c19ad0877649
vshetty28 <vshetty1028@gmail.com>
2025-02-08 02:42:12 -0500

    skill light mode edit

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Skill = ({icon, text}) => {
   return (
 		<div>
-			<div className="group flex flex-row bg-base-100 p-1 px-2 items-center rounded-lg border-black border-2 transition delay-0 duration-200 ease-in-out hover:border-2 hover:border-primary ">
+			<div className="group flex flex-row bg-base-100 p-1 px-2 items-center rounded-lg dark:border-black border-2 transition delay-0 duration-200 ease-in-out hover:border-2 hover:border-primary ">
 				<Image src={icon} alt="" className="size-4 lg:size-8 m-1 lg:m-2" width={50} height={50} />
 				<h1 className=" text-xs lg:text-lg px-2 transition delay-0 duration-200 font-bold font-sans group-hover:scale-110 ease-in-out select-none">{text}</h1>
 			</div>
