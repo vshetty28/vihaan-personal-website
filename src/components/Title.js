d8038d3d5e19b2a9edf5be1db2bf7eca4f13827e
vshetty28 <vshetty1028@gmail.com>
2025-02-08 01:43:19 -0500

    Title centering mobile fix

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Title = ({text}) => {
-	return <h1 className="max-w-fit bg-inherit p-4 py-6 text-3xl lg:text-6xl lg:mb-8 justify-self-center text-center font-bold bg-primary dark:bg-gradient-to-br from-primary to-primary lg:to-indigo-800 text-transparent bg-clip-text tracking-wide border-primary rounded-md">{text}</h1>;
+	return <h1 className="min-w-screen lg:min-w-fit bg-inherit p-4 py-6 text-3xl lg:text-6xl lg:mb-8 justify-self-center text-center font-bold bg-primary dark:bg-gradient-to-br from-primary to-primary lg:to-indigo-800 text-transparent bg-clip-text tracking-wide border-primary rounded-md">{text}</h1>;
 };
 
 export default Title;
