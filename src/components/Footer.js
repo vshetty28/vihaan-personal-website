80bcca665fcf2e153c4200be3b9e6176774c4ee2
vshetty28 <vshetty1028@gmail.com>
2025-02-13 15:51:08 -0500

    updated email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
 			<footer className="footer min-w-screen items-center justify-between px-6">
 				<h1 className="invisible lg:visible lg:text-2xl lg:mt-4 font-bold bg-gradient-to-tr from-primary to-indigo-700 text-transparent bg-clip-text">Contact Me</h1>
 				<div className="flex flex-row">
-					<a href="mailto:vihaandhshetty@gmail.com">
+					<a href="mailto:vihaanshetty@gmail.com">
 						<Image src="/assets/email.svg" alt="" className="size-8 lg:size-14 hover:scale-110 mt-5 lg:mt-2 transition delay-0 duration-200 ease-in-out" width={50} height={80} />
 					</a>
 					<a href="https://linkedin.com/in/vihaanshetty" target="_blank">
