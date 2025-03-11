5104b7cd915e68334fe08bdd64b9ac7eb15d1207
vshetty28 <vshetty1028@gmail.com>
2025-03-10 20:18:50 -0400

    firefox image formatting fix

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -10,9 +10,9 @@ const AboutMe = () => {
 			</Head>
 			<div className="hero bg-inherit min-w-screen">
 				<div className="hero-content flex-col lg:flex-row-reverse">
-					<Image src="/assets/headshot.png" alt="" className="size-1/2 lg:size-1/4 rounded-full shadow-2xl max-h-full lg:m-4" width={4000} height={4000} />
-					<div className="lg:px-20 lg:mx-10">
-            <Title text='About Me' />
+					<Image src="/assets/headshot.png" alt="Headshot" className="flex-2 size-1/2 lg:size-1/4 rounded-full shadow-2xl max-h-full lg:m-4" width={1000} height={1000} />
+					<div className="lg:px-20 lg:mx-10 flex-1">
+						<Title text="About Me" />
 						<p className="text-center text-balance lg:text-left text-sm lg:text-xl">Hi! My name is Vihaan Shetty. Welcome to my personal website! I am originally from the SF Bay Area, California, but am currently studying Computer Science and Data Science at Purdue. I expect to graduate in Spring 2026. I am a huge sports fan, with some of my favorite teams being the 49ers, Warriors, FC Barcelona and Liverpool FC. In my free time, I love working out and playing videogames. Feel free to contact me at the links provided below, or explore the rest of the site!</p>
 					</div>
 				</div>
