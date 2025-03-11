9183f8c777b9e919a59a3a77d03683790e48ebe1
vshetty28 <vshetty1028@gmail.com>
2025-03-10 21:54:41 -0400

    added new skills

--- src/data/data.js
+++ src/data/data.js
@@ -16,7 +16,7 @@ export const frameworks = [
 	{ icon: "/assets/node.svg", text: "Node.js" },
 	{ icon: "/assets/pandas.svg", text: "Pandas" },
 	{ icon: "/assets/flask.svg", text: "Flask" },
-	{ icon: "/assets/sklearn.svg", text: "sk-learn" },
+	{ icon: "/assets/scikit-learn.svg", text: "sk-learn" },
 	{ icon: "/assets/tensorflow.svg", text: "TensorFlow" },
 	{ icon: "/assets/numpy.svg", text: "NumPy" },
 	{ icon: "/assets/postgresql.svg", text: "PostgreSQL" },
@@ -28,11 +28,12 @@ export const frameworks = [
 	{ icon: "/assets/google_cloud.svg", text: "Google Cloud" },
 	{ icon: "/assets/react_native.svg", text: "React Native" },
 	{ icon: "/assets/bash.svg", text: "bash" },
+	{ icon: "/assets/expo.svg", text: "Expo" },
+	{ icon: "/assets/django.svg", text: "Django" },
 ];
 
 export const experiences = [
-	{ id: 1, date: "Aug 2023 - May 2024", title: "Undergraduate Researcher - PFF", company: "The Data Mine", location: 'Purdue University', description: "- Developed ML algorithms using XGBoost on advanced NFL charting data to analyze value, difficulty, and frequency of NFL quarterback throws\n\n- Trained models on Pro Football Focus's NFL data and applied to NCAAF data for applications in pre-draft analysis\n\n- Created interactive Tableau visualizations of algorithm results and other patterns found in data", start: true, end: false, completed: true },
-	{ id: 2, date: "Jan 2024 - Present", title: "Corporate Partners TA", company: "The Data Mine", location: 'Purdue University', description: "- Project Manager overseeing Data Mine students working alongside the Management Performance Hub on ICJI data\n\n- Responsible for enforcing Agile methodology as Scrum Master\n\n- Helped students with technical issues and assisted in their development of technical skills required for the project", start: false, end: false, completed: true },
-	{ id: 3, date: "Summer 2025", title: "Data Science Engineer Intern", company: "BNY Mellon", location:'New York City, NY', description: "Starts in Summer 2025", start: false, end: true, completed: false },
+	{ id: 1, date: "Aug 2023 - May 2024", title: "Undergraduate Researcher - PFF", company: "The Data Mine", location: "Purdue University", description: "- Developed ML algorithms using XGBoost on advanced NFL charting data to analyze value, difficulty, and frequency of NFL quarterback throws\n\n- Trained models on Pro Football Focus's NFL data and applied to NCAAF data for applications in pre-draft analysis\n\n- Created interactive Tableau visualizations of algorithm results and other patterns found in data", start: true, end: false, completed: true },
+	{ id: 2, date: "Jan 2024 - Present", title: "Corporate Partners TA", company: "The Data Mine", location: "Purdue University", description: "- Project Manager overseeing Data Mine students working alongside the Management Performance Hub on ICJI data\n\n- Responsible for enforcing Agile methodology as Scrum Master\n\n- Helped students with technical issues and assisted in their development of technical skills required for the project", start: false, end: false, completed: true },
+	{ id: 3, date: "Summer 2025", title: "Data Science Engineer Intern", company: "BNY Mellon", location: "New York City, NY", description: "Starts in Summer 2025", start: false, end: true, completed: false },
 ];
-
