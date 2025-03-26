ddccc3b7567842254eb6b13fa9e4f9a34bb0046f
vshetty28 <vshetty1028@gmail.com>
2025-03-25 21:33:23 -0400

    visual tweaks

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Skill from './Skill'
-
+import {motion} from 'motion/react'
 const SkillList = ({name, skills}) => {
   return (
-		<div className=''>
-			<h1 className='lg:p-2 m-2 place-self-center text-center font-bold text-lg lg:text-3xl tracking-normal'>{name}</h1>
-			<div className="flex flex-row gap-2 items-center flex-wrap justify-center p-2 lg:px-72 lg:p-4">
+		<div className="">
+			<motion.h1 initial={{ opacity: 0, scale:1, y: 50 }} animate={{ opacity: 1, y: 0, scale: 1, transition: { duration: 0.8, ease: "easeInOut", delay: 0.1 } }} className="lg:p-2 m-2 place-self-center text-center font-bold text-lg lg:text-3xl tracking-normal">
+				{name}
+			</motion.h1>
+			<motion.div initial={{ opacity: 0, scale:0.5, y: 50}} animate={{ opacity: 1, scale: 1, y:0, transition: { duration: 1, ease: "easeInOut", delay: 0.8} }} className="flex flex-row gap-2 items-center flex-wrap justify-center p-2 lg:px-72 lg:p-4">
 				{skills.map((skill) => (
 					<Skill key={skill.text} icon={skill.icon} text={skill.text} />
 				))}
-			</div>
+			</motion.div>
 		</div>
 	);
 }
--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -12,12 +12,8 @@ const Skills = () => {
 			</Head>
 			<Title text="Technical Skills" />
 			<div className="flex flex-col justify-items-center">
-				<motion.div initial={{ opacity: 0, x: -150 }} animate={{ opacity: 1, x: 0, transition: { duration: 1, ease: "easeInOut", delay: 0.6 } }}>
-					<SkillList name="Programming Languages" skills={programmingLanguages} />
-				</motion.div>
-				<motion.div initial={{ opacity: 0, x: -150 }} animate={{ opacity: 1, x: 0, transition: { duration: 1, ease: "easeInOut", delay: 1.4 } }}>
-					<SkillList name="Frameworks/Technologies" skills={frameworks} />
-				</motion.div>
+				<SkillList name="Programming Languages" skills={programmingLanguages} />
+				<SkillList name="Frameworks/Technologies" skills={frameworks}/>
 			</div>
 		</div>
 	);
