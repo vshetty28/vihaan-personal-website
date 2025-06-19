7dac2540e630b71c06bfb8fa0d7108d330c0fdad
vshetty28 <vshetty1028@gmail.com>
2025-06-19 00:46:49 -0400

    modified message

--- public/update_resume.sh
+++ public/update_resume.sh
@@ -1,5 +1,5 @@
 cp -rf ~/Documents/Personal/Resume/Vihaan_Shetty_Resume.pdf resume.pdf
 git add resume.pdf
-git commit -m 'Resume Update'
+git commit -m 'Resume update'
 git push origin main
 npm run build
