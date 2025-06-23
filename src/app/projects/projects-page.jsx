307592c7e93795f0984fbf9920d76e46db9b05d7
vshetty28 <vshetty1028@gmail.com>
2025-06-22 23:40:08 -0400

    project base structure

--- src/app/projects/projects-page.jsx
+++ src/app/projects/projects-page.jsx
@@ -1,13 +1,19 @@
 "use client";
 import Title from "@/components/Title";
 import { motion } from "motion/react";
+import Project from "@/components/Project";
+import { projects } from "@/data/data";
 
 const ProjectsPage = () => {
 	return (
 		<div className="text-center w-screen">
 			<Title text="Projects" />
 			<motion.div initial={{ opacity: 0, x: -250 }} animate={{ opacity: 1, x: 0, transition: { duration: 0.8, ease: "easeInOut", delay: 0.8 } }}>
-				Under Construction. Please check back soon!
+				<div className="grid auto-rows-max auto-cols-max grid-cols-1 lg:grid-cols-3 gap-4 p-5 items-stretch">
+					{projects.map((project) => (
+						<Project key={project.id} project={project} />
+					))}
+				</div>
 			</motion.div>
 		</div>
 	);
---
+++ src/components/Project.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+const Project = ({ project }) => {
+	return (
+		<div className="flex flex-col h-full border-2 border-gray-700 rounded-md items-center justify-start pb-4 px-4 gap-3">
+			<h1 className="text-3xl font-bold mt-4 bg-gradient-to-r from-purple-500 to-primary text-transparent bg-clip-text">{project.name}</h1>
+			<div className="flex-0 flex flex-row flex-wrap justify-center gap-2">
+				{project.skills.map((skill, index) => (
+					<span key={index} className="bg-primary text-black text-xs font-semibold px-2 py-0.5 rounded-md">
+						{skill}
+					</span>
+				))}
+			</div>
+			<div className="flex-2 flex-grow text-center self-center">
+				<h1 className="text-sm text-center text-pretty">{project.description}</h1>
+			</div>
+			{project.github ? (
+				<div className="flex-0 flex items-center justify-center gap-2">
+					<button className="btn btn-outline">
+						<Image src="/assets/github.svg" alt="GitHub Icon" width={24} height={24} className="" />
+						<a href={project.github} target="_blank" rel="noopener noreferrer">
+							GitHub
+						</a>
+					</button>
+				</div>
+			) : (
+				<div className="flex-1 btn btn-outline btn-disabled border-2 justify-self-end mt-2">
+					<div className="flex items-center justify-center gap-2">
+						<Image src="/assets/github.svg" alt="GitHub Icon" width={24} height={24} className="" />
+						<h1>GitHub Not Available</h1>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Project;
